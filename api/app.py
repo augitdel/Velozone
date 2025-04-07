706726be856e2324c914812335a40e63822496df
audlbeke <august.delbeke@ugent.be>
2025-04-07 15:21:23 +0200

    fix last lines off app.py

--- api/app.py
+++ api/app.py
@@ -221,5 +221,5 @@ def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static/favicon'), 'favicon.ico', mimetype='image/vnd.microsoft.icon') 
 
 if __name__ == '__main__':
-    app.run(debug=True)
     monitor_thread = Thread(target=start_monitor_thread, daemon=True)
+    app.run(debug=True)
